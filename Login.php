4fa455f7c8f43792969f1c4ade4aca61536679f9
Samuel Levin <levinsamuel000@ucla.edu>
2012-01-14 23:58:32 -0800

    Edited Student view and Login, check README

--- Login.php
+++ Login.php
@@ -1,40 +1,22 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<html><head>
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+  <title>Login</title>
 
-<head>
-    <link rel="stylesheet" type="text/css" href="Login.css" />
-    <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-    <title>Login</title>
-    
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  
+  
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+</head><body>
+<h1> Students: </h1>
 
-</head>
+<form name="input" action="StudentView.php" method="get"> Full
+name: <input name="Name" value="Enter your name here" type="text"> <br>
+  <input value="Submit" type="submit"></form>
 
-<body>
-    <h1> Welcome! </h1>
-    
-    <hr style="width: 100%; height: 2px;">
-        
-    <h2> Students: </h2>
-    
-    <div id = "logon">
-    <form name="input" action="" method="get">
-        Full name: <input name="Name" value="" type="text">
-        <input value="Submit" type="submit">
-    </form>
-    </div>
-    
-    <br> <br>
-    
-    <h3> Teachers: </h3>
+<h1> Teachers: </h1>
 
-    <div id = "logon">
-        <form action="passwordcheck.php" method="post">
-            Password: <input type="password" name="mypassword">
-            <input type=submit value="submit">
-        </form>
-    </div>
-    
-</body>
+<form name="input" action="html_form_action.asp" method="get"> <input name="Password" value="Enter your password" type="text"> <br>
+  <input value="Submit" type="submit"></form>
 
-</html>
+</body></html>
--- StudentView.php
+++ StudentView.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
+<html><head>
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+  <title>Student: Write your story!</title>
 
-<head>
-    <link rel="stylesheet" type="text/css" href="StudentView.css" />
-    <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-    <title>Student: Write your story!</title>
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-</head>
-
-<body>
-    <h1> Write your story! </h1>
-</body>
-
-</html>
+  
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+</head><body>
+<h> Write your story! </h>
+<?php
+	$student = htmlspecialchars($_GET['Name']);
+	
+	echo $student
+	
+	?>
+</body></html>
